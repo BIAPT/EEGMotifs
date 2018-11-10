b854129728765782e5249a70c7b7deafe458aaeb
Bl0101 <yacine.mahdid@live.com>
2018-11-10 18:55:54 -0500

    ADD get_number_participant

--- lib/EEGmotifs_code/main/crawl.m
+++ lib/EEGmotifs_code/main/crawl.m
@@ -7,14 +7,14 @@ function crawl(information,parameters)
     loading_path = information.loading_path;
     saving_path = information.saving_path;
     FOLDER_NAME = strcat("EEGMotifs_",num2str(floor(now*100000)));
+    number_participant = get_number_participant(loading_path);
     
-
     %% Create the base folder at right place
     mkdir(saving_path,FOLDER_NAME);    
     information.saving_path = strcat(saving_path,filesep,FOLDER_NAME);
     
-    %% TODO: Start the workers here
-    
+    %% Start the workers here
+    cluster_pool = gcp();
     %% Crawling through the data
     
     % TODO: Need to crawl through each participant (FOR)
