b91f5b1243c86ec188082afb0b3fb333fa17e886
Bl0101 <yacine.mahdid@live.com>
2019-02-06 10:49:14 -0500

    ADD function header for random network validation

---
+++ src/testing/random_network_validation.m
@@ -0,0 +1,7 @@
+function [validated_network] = random_network_validation(original_network,...
+                                p_value,rewiring_parameter,number_random_network)
+%RANDOM_NETWORK_VALIDATION Summary of this function goes here
+%   Detailed explanation goes here
+
+end
+
