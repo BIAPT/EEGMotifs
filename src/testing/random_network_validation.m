2defdeb87df7b924a94e39fea1e0fa177726f711
Bl0101 <yacine.mahdid@live.com>
2019-02-07 10:10:54 -0500

    ADD network validation returning motifs

--- src/testing/random_network_validation.m
+++ src/testing/random_network_validation.m
@@ -1,16 +1,23 @@
-function [z_score_frequency] = random_network_validation(og_network,...
-                                p_value,rewiring,number_rand_network)
+function [z_score_frequency,og_motifs,rand_motifs] = random_network_validation(og_network,...
+                                                    p_value,rewiring,number_rand_network)
 %RANDOM_NETWORK_VALIDATION Summary of this function goes here
 %   Detailed explanation goes here
 
     % Function will be using randmio_dir.m from the BCT toolbox
     
-    %% Note: Here we only will use the weighted networks
+    %% Variable Initialization
+    og_motifs = struct();
+    rand_motifs = struct();
+    
     %% 0) Thresholding to make the Brain Connectivity Toolbox works
     [og_network] = threshold(og_network,"dpli",0,0);
     %% 1) Calculate the motif for our network of interest.
     disp('Calculate Motif for NOI')
     [og_intensity,og_coherence,og_frequency] = motif3funct_wei(og_network);
+    % Saving the motifs for the return
+    og_motifs.intensity = og_intensity;
+    og_motifs.coherence = og_coherence;
+    og_motifs.frequency = og_frequency;
     
     rand_intensity = zeros(number_rand_network,13,length(og_network));
     rand_coherence = zeros(number_rand_network,13,length(og_network));
@@ -26,17 +33,17 @@ function [z_score_frequency] = random_network_validation(og_network,...
         [rand_intensity(i,:,:),rand_coherence(i,:,:),rand_frequency(i,:,:)] = motif3funct_wei(rand_network);
     end
     
+    % Saving the random motifs for the return
+    rand_motifs.intensity = rand_intensity;
+    rand_motifs.coherence = rand_coherence;
+    rand_motifs.frequency = rand_frequency;
+    
     %% 4) Calculate the Z score for each motifs
     disp('Calculate Z score')
-    [z_score_frequency] = calculate_z_score(og_frequency,rand_frequency);
-    %% 5) Flag as statistically significant the motifs that have a Z = 1.96 (p < 0.05)
-    disp('Threshold value that are not statistically significant')
-end
-
-function [z_score_frequency] = calculate_z_score(og_frequency,rand_frequency)
     rand_frequency_mean = squeeze(mean(rand_frequency));
     rand_frequency_std = squeeze(std(rand_frequency));
-    %% Here will use the formula
-    z_score_frequency = (og_frequency - rand_frequency_mean) ./ rand_frequency_std
+    z_score_frequency = (og_frequency - rand_frequency_mean) ./ rand_frequency_std;
+    %% 5) Flag as statistically significant the motifs that have a Z = 1.96 (p < 0.05)
+    disp('Threshold value that are not statistically significant')
 end
 
