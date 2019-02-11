95b29cacfb7fbe3b04f3ae2fda307c8bad3f0d79
Bl0101 <yacine.mahdid@live.com>
2019-02-11 15:19:33 -0500

    Refactor random network validation

--- src/testing/random_network_validation.m
+++ src/testing/random_network_validation.m
@@ -1,4 +1,4 @@
-function [motifs] = random_network_validation(og_network,rewiring,number_rand_network)
+function [motifs] = random_network_validation(network,rewiring,number_rand_network)
 %RANDOM_NETWORK_VALIDATION calculate motifs and z score for frequency
 %   Detailed explanation goes here
 
@@ -18,30 +18,33 @@ function [motifs] = random_network_validation(og_network,rewiring,number_rand_ne
     %   - node_frequency
     %   - node_intensity
     %   - node_coherence
+    % 
+    %   - z score frequency
+    
     
     %% Variable Initialization
     motifs = struct();
-    motifs = struct();
     
     %% 0) Thresholding to make the Brain Connectivity Toolbox works
-    [og_network] = threshold(og_network,"dpli",0,0);
+    [network] = threshold(network,"dpli",0,0);
     
     %% 1) Calculate the motif for our network of interest.
-    [og_intensity,og_coherence,og_frequency] = motif3funct_wei(og_network);
+    [intensity,coherence,frequency] = motif3funct_wei(network);
     % Saving the motifs for the return
-    motifs.raw_intensity = og_intensity;
-    motifs.raw_coherence = og_coherence;
-    motifs.raw_frequency = og_frequency;
+    motifs.raw_intensity = intensity;
+    motifs.raw_coherence = coherence;
+    motifs.raw_frequency = frequency;
     
-    rand_intensity = zeros(number_rand_network,13,length(og_network));
-    rand_coherence = zeros(number_rand_network,13,length(og_network));
-    rand_frequency = zeros(number_rand_network,13,length(og_network)); 
+    % Create the matrices
+    rand_intensity = zeros(number_rand_network,13,length(network));
+    rand_coherence = zeros(number_rand_network,13,length(network));
+    rand_frequency = zeros(number_rand_network,13,length(network)); 
     
     disp('Create Random Network')
     %% 2) Create X random network using our network of interest 
     for i = 1:number_rand_network
         disp(strcat("network: ",string(i)))
-        [rand_network,~]=randmio_dir(og_network, rewiring);
+        [rand_network,~] = randmio_dir(network, rewiring);
         %% 3) Calculate the motif for the X random network. (BOTTLE NECK)
         [rand_intensity(i,:,:),rand_coherence(i,:,:),rand_frequency(i,:,:)] = motif3funct_wei(rand_network);
     end
@@ -56,24 +59,24 @@ function [motifs] = random_network_validation(og_network,rewiring,number_rand_ne
     disp('Calculate Z score')
     rand_frequency_mean = mean(sum(rand_frequency,3))';
     rand_frequency_std = std(sum(rand_frequency,3))';
-    og_frequency_sum = sum(og_frequency,2);
-    z_score_frequency = (og_frequency_sum - rand_frequency_mean) ./ rand_frequency_std;
+    frequency_sum = sum(frequency,2);
+    z_score_frequency = (frequency_sum - rand_frequency_mean) ./ rand_frequency_std;
     
     %% 5) Flag as statistically significant the motifs that have a Z = 1.96 (p < 0.05)
     disp('Threshold value that are not statistically significant')
     for i=1:13
        % Need to set the count for these motifs to 0 
        if(z_score_frequency(i) < 1.96) 
-           for j=1:length(og_frequency)
-              og_frequency(i,j) = 0;
+           for j=1:length(frequency)
+              frequency(i,j) = 0;
            end
        end
     end
     
     % Here we set the thresholded values
-    motifs.node_frequency = og_frequency;
-    motifs.node_intensity = og_intensity;
-    motifs.node_coherence = og_coherence;
+    motifs.node_frequency = frequency;
+    motifs.node_intensity = intensity;
+    motifs.node_coherence = coherence;
     
     % Save the z_score for the frequency
     motifs.z_score_frequency = z_score_frequency;
