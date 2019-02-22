8c8ac9714b4084b77ffaf7376812e0ab9b00e166
yacine <yacine.mahdid@mail.mcgill.ca>
2019-02-22 12:35:17 -0500

    Update investigate_the_std.m

--- src/testing/investigate_the_std.m
+++ src/testing/investigate_the_std.m
@@ -1,5 +1,5 @@
 %% TODO: investigate how to calculate the std properly using a 129 000x13 matrices.
-rand_network = rand(100,13,129);
+rand_network = motifs.rand_frequency;
 number_rand_network = 100;
 cat_rand_network = rand_network(1,:,:);
 for i = 2:number_rand_network
@@ -9,4 +9,4 @@ cat_rand_network = squeeze(cat_rand_network);
 
 std_rand_network = std(cat_rand_network');
 
-mean_rand_network = mean(mean(rand_network,3));
+mean_rand_network = mean(cat_rand_network');
