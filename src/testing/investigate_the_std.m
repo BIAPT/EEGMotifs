520a49b02543d0af31afae0b8b12d319da8a75b3
Bl0101 <yacine.mahdid@live.com>
2019-02-22 09:38:48 -0500

    Concatenate the random networks

--- src/testing/investigate_the_std.m
+++ src/testing/investigate_the_std.m
@@ -1,1 +1,9 @@
 %% TODO: investigate how to calculate the std properly using a 129 000x13 matrices.
+rand_network = rand(100,13,129);
+number_rand_network = 100;
+cat_rand_network = rand_network(1,:,:);
+for i = 2:number_rand_network
+    cat_rand_network = cat(3,cat_rand_network,rand_network(i,:,:));
+end
+cat_rand_network = squeeze(cat_rand_network);
+std_rand_network = std(cat_rand_network');
