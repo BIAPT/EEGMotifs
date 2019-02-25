bd26e899d86b0d93f2a1666316fa8b8e6796f367
Bl0101 <yacine.mahdid@live.com>
2019-02-25 11:43:46 -0500

    ADD std fix

--- src/testing/investigate_the_std.m
+++ src/testing/investigate_the_std.m
@@ -1,5 +1,5 @@
 %% TODO: investigate how to calculate the std properly using a 129 000x13 matrices.
-rand_network = rand(100,13,129);
+rand_network = motifs.rand_frequency;
 number_rand_network = 100;
 cat_rand_network = rand_network(1,:,:);
 for i = 2:number_rand_network
@@ -10,3 +10,9 @@ cat_rand_network = squeeze(cat_rand_network);
 std_rand_network = std(cat_rand_network');
 
 mean_rand_network = mean(mean(rand_network,3));
+
+for i=1:13
+   figure();
+   hist(cat_rand_network(i,:),100);
+   title(["Null networks histogram for motif # ",string(i)]);
+end
