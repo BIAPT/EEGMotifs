1e2979d341f970d5ec81e9aff2a8d69b0493e22c
Bl0101 <yacine.mahdid@live.com>
2019-03-04 12:59:09 -0500

    ADD skip channels in aggregate function

--- src/testing/aggregate.m
+++ src/testing/aggregate.m
@@ -54,29 +54,34 @@ function [dataset] = pad(dataset,index)
     E126 E127 E128
     
     %}
+    skip_array = [E1 E8 E14 E17 E21 E25 E32 E38 E43 E44 E48 E49 E56 E57 E63 E64 E68 E69 E73 E74 
+    E81 E82 E88 E89 E94 E95 E99 E100 E107 E113 E114 E119 E120 E121 E125 E126 E127 E128];
     zero_array = [];
     if(index == 3)
-        zero_array = [8,13,23,45,56,57,63,107,115];
+        zero_array = [13,23,45,56,115];
     elseif(index == 4)
-        zero_array = [17,18,22,40,44,56,100,107,123];
+        zero_array = [18,22,40,123];
     elseif(index == 5)
-        zero_array = [3,10,11,45,57,70,71,102,110,111,126];
+        zero_array = [3,10,11,45,70,71,102,110,111];
     elseif(index == 6)
         zero_array = [46,47,55];      
     elseif(index == 8)
-        zero_array = [100];        
+        zero_array = [];        
     elseif(index == 9)
-        zero_array = [8,21,25,81,88];
+        zero_array = [];
     else
         return
     end
     
-    frequency = zeros(13,129);
-    intensity = zeros(13,129);
+    frequency = zeros(13,91);
+    intensity = zeros(13,91);
     
     %% Here we pad when we need to
     index = 1;
     for i = 1:129
+        if(ismember(i,skip_array))
+            continue
+        end
         if(ismember(i,zero_array))
             frequency(:,i) = zeros(13,1);
             intensity(:,i) = zeros(13,1);
