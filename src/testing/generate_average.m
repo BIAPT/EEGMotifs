3721ea1c90c0e0ab47c07140a00c3f70c6cc203e
Bl0101 <yacine.mahdid@live.com>
2019-03-19 14:36:57 -0400

    ADD aggregate struct generation

--- src/testing/generate_average.m
+++ src/testing/generate_average.m
@@ -1,13 +1,33 @@
 % Scripts used to generate the average participant given the pre-calculated
 % motifs. Make use of aggregate and average script.
 
+%% Variables Declaration
+number_participant = 9;
+number_epoch = 10;
+number_channels = 129;
+epochs = {'EC1','IF5','EMF5','EML5','EC3','EC4','EC5','EC6','EC7','EC8'};
+
 %% Step 1: Iterate throught the data and load it into a structure
 % First we should let the user select a folder where the data is located.
 [data_path] = uigetdir('Select Motifs Results Folder');
 % Then we should get every folder inside that folder
 all_dir = get_all_directory(data_path);
-
-
+% After that we should iterate through every folder and load the data into
+% a data structure that will contain every data points separated by epoch
+data_motifs = [];
+% each row of this struct is an epoch that has a name and a frequency_data
+% 3d array
+for i=1:number_epoch
+    % Create the structure
+    data_struct = struct();
+    data_struct.name = epochs{i};
+    data_struct.frequency_data = zeros(number_participant,number_channels,number_channels);
+    
+    % Populate it
+    
+    % Save it into data_motifs
+    data_motifs = [data_motifs,data_struct];
+end
 
 % Helper functions
 % Get all the dirs and process them to have the same structure as a call
@@ -31,6 +51,8 @@ function [all_dir] = get_all_directory(data_path)
           all_dir(i) = [];
           i = i - 1;
           number_directory = number_directory - 1;
+       else % we add the index
+           all_dir(i).index = fetch_index(name);
        end
        
        %
@@ -39,15 +61,26 @@ function [all_dir] = get_all_directory(data_path)
     
 end
 
+% Return an index corresponding to the type of datasets
 function [index] = fetch_index(name)
-    % Here are the index and what they mean
-    % 1 = MDAF03
-    % 2 = MDAF05
-    % 3 = MDAF06
-    % 4 = MDAF07
-    % 5 = MDAF10
-    % 6 = MDAF11
-    % 7 = MDAF12
-    % 8 = MDAF15
-    % 9 = MDAF17 
+    index = -1;
+    if(strcmp(name,"MDFA03"))
+        index = 1;
+    elseif(strcmp(name,"MDFA05"))
+        index = 2;
+    elseif(strcmp(name,"MDFA06"))
+        index = 3;
+    elseif(strcmp(name,"MDFA07"))
+        index = 4;
+    elseif(strcmp(name,"MDFA10"))
+        index = 5;
+    elseif(strcmp(name,"MDFA11"))
+        index = 6;
+    elseif(strcmp(name,"MDFA12"))
+        index = 7;
+    elseif(strcmp(name,"MDFA15"))
+        index = 8;
+    elseif(strcmp(name,"MDFA17"))
+        index = 9;
+    end
 end
