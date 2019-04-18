45f991bd5ce4994126081e0c8d46420a2051370a
Bl0101 <yacine.mahdid@live.com>
2019-04-18 13:43:09 -0400

    ADD a bunch of figures

--- src/testing/cosine_similarity/investigate_cosine.m
+++ src/testing/cosine_similarity/investigate_cosine.m
@@ -3,10 +3,20 @@
 % Parameters
 motif = 2;
 num_epoch = 12;
+sift = -1; % sift = 0 , means full head ; sift = 1 , means anterior ; sift = -1 means posterior
 epoch_labels = {'EC1','IF5','EF5','EL30','EL10','EL5','EC3','EC4','EC5','EC6','EC7','EC8'};
+sift_label = 'Full';
+if(sift == 1)
+   sift_label = 'Anterior'; 
+elseif(sift == -1)
+    sift_label = 'Posterior';
+end
 
 data = load('average_data.mat');
 data_motifs = data.data_motifs;
+
+data = load('EEG_info_AVG.mat');
+chanlocs = data.EEG_info.chanlocs;
 
 alpha_cosim_matrix = zeros(num_epoch,num_epoch);
 theta_cosim_matrix = zeros(num_epoch,num_epoch);
@@ -24,16 +34,43 @@ for epoch_i = 1:num_epoch
    for epoch_j = 1:num_epoch
        A = alpha_vector(epoch_i,:);
        B = alpha_vector(epoch_j,:);
+       
+       % Sifting the two vectors
+       A = sift_anterior_posterior(A,chanlocs,sift);
+       B = sift_anterior_posterior(B,chanlocs,sift);       
        alpha_cosim_matrix(epoch_i,epoch_j) = cosine_similarity(A,B);
        
        A = theta_vector(epoch_i,:);
        B = theta_vector(epoch_j,:);
+       
+       % Sifting the two vectors
+       A = sift_anterior_posterior(A,chanlocs,sift);
+       B = sift_anterior_posterior(B,chanlocs,sift);  
        theta_cosim_matrix(epoch_i,epoch_j) = cosine_similarity(A,B);
    end
 end
 
-plot_cosine_similarity(alpha_cosim_matrix,sprintf('Cosine similarity | Alpha | Average Participant | Motif %d',motif),epoch_labels);
-plot_cosine_similarity(theta_cosim_matrix,sprintf('Cosine similarity | Theta | Average Participant | Motif %d',motif),epoch_labels);
+plot_cosine_similarity(alpha_cosim_matrix,sprintf('Cosine similarity | %s | Alpha | Average Participant | Motif %d',sift_label,motif),epoch_labels);
+plot_cosine_similarity(theta_cosim_matrix,sprintf('Cosine similarity | %s | Theta | Average Participant | Motif %d',sift_label,motif),epoch_labels);
+
+function [sifted_vector] = sift_anterior_posterior(vector,chanlocs,sift)
+    sifted_vector = [];
+    if(sift == 0)
+        sifted_vector = vector;
+    else
+        
+        for i = 1:length(vector)
+           if(sift == 1 && chanlocs(i).X > -0.001)
+               disp(chanlocs(i).labels)
+               sifted_vector = [sifted_vector, vector(i)];
+           elseif(sift == -1 && chanlocs(i).X < 0.001)
+               disp(chanlocs(i).labels)
+               sifted_vector = [sifted_vector, vector(i)];
+           end
+        end
+        
+    end
+end
 
 function plot_cosine_similarity(cosim_matrix,title_string,ticks_string)
     figure;
--- src/testing/cosine_similarity/investigate_frequency_count.m
+++ src/testing/cosine_similarity/investigate_frequency_count.m
@@ -1,8 +1,15 @@
 % Parameters
-motif = 2;
+motif = 7;
 num_epoch = 12;
 num_pariticipant = 9;
-frequency_band = 1; % 1 = alpha, 2 = theta
+frequency_band = 2; % 1 = alpha, 2 = theta
+sift = -1; % sift = 0 , means full head ; sift = 1 , means anterior ; sift = -1 means posterior
+sift_label = 'Full';
+if(sift == 1)
+   sift_label = 'Anterior'; 
+elseif(sift == -1)
+    sift_label = 'Posterior';
+end
 if(frequency_band == 1)
    frequency = 'alpha'; 
 else
@@ -13,20 +20,27 @@ epoch_labels = {'EC1','IF5','EF5','EL30','EL10','EL5','EC3','EC4','EC5','EC6','E
 data = load('average_data.mat');
 data_motifs = data.data_motifs;
 
+data = load('EEG_info_AVG.mat');
+chanlocs = data.EEG_info.chanlocs;
+
 X = zeros(num_pariticipant,num_epoch);
-means = zeros(1,num_epoch);
-stds = zeros(1,num_epoch);
 for i=1:num_epoch
    epoch_data = data_motifs(i);
-   frequency_data = sum(squeeze(epoch_data.frequency_data(frequency_band,:,motif,:)),2);
-   means(1,i) = mean(frequency_data);
-   stds(1,i) = std(frequency_data);
+   all_frequency_data = squeeze(epoch_data.frequency_data(frequency_band,:,motif,:));
+   sifted_frequency_data = [];
+   for j = 1:num_pariticipant
+      participant_frequency_data = all_frequency_data(j,:);
+      sifted_frequency_data = [ sifted_frequency_data ;  sift_anterior_posterior(participant_frequency_data,chanlocs,sift)];
+   end
+   
+   frequency_data = sum(sifted_frequency_data,2);
+
    X(:,i) = frequency_data;
    
 end
 
 
-plot_average_frequency_count(X,sprintf("Boxplot Sum Channels Frequency | %s | Motif %d",frequency,motif),epoch_labels);
+plot_average_frequency_count(X,sprintf("Boxplot Sum Channels Frequency | %s | %s | Motif %d",sift_label,frequency,motif),epoch_labels);
 
 function plot_average_frequency_count(X,title_string,ticks_string)
     figure;
@@ -36,4 +50,23 @@ function plot_average_frequency_count(X,title_string,ticks_string)
     ylabel('Frequency Count')
     xticks(1:length(X))
     xticklabels(ticks_string)
+end
+
+function [sifted_vector] = sift_anterior_posterior(vector,chanlocs,sift)
+    sifted_vector = [];
+    if(sift == 0)
+        sifted_vector = vector;
+    else
+        
+        for i = 1:length(vector)
+           if(sift == 1 && chanlocs(i).X > -0.001)
+               disp(chanlocs(i).labels)
+               sifted_vector = [sifted_vector, vector(i)];
+           elseif(sift == -1 && chanlocs(i).X < 0.001)
+               disp(chanlocs(i).labels)
+               sifted_vector = [sifted_vector, vector(i)];
+           end
+        end
+        
+    end
 end
